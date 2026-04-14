c52389f1bbcfe40cd728b79c8ae0df8051e7066c
Brunno Felipe Bezerra <brunnofdev@gmail.com>
2026-04-14 11:12:24 -0300

    Feat: Primeiro Commit

---
+++ src/main/java/br/com/ifpb/adotaifpb/AdotaIfpbApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpb.adotaifpb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdotaIfpbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdotaIfpbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/ifpb/adotaifpb/AdotaIfpbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpb.adotaifpb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdotaIfpbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
