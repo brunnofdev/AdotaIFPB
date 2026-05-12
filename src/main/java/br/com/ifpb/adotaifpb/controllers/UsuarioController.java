5540dca42ebd12946b05a25d18bd194d9f08d0a9
ArthurHDEV <arthurhap.pro@gmail.com>
2026-05-12 11:33:35 -0300

    feat: criação de UsuarioController

---
+++ src/main/java/br/com/ifpb/adotaifpb/controllers/UsuarioController.java
@@ -0,0 +1,54 @@
+package br.com.ifpb.adotaifpb.controllers;
+
+import br.com.ifpb.adotaifpb.dtos.UsuarioRequestDTO;
+import br.com.ifpb.adotaifpb.dtos.UsuarioResponseDTO;
+import br.com.ifpb.adotaifpb.services.UsuarioService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<?> cadastrarUsuario(@Valid @RequestBody UsuarioRequestDTO usuariodto) {
+        try {
+            UsuarioResponseDTO novoUsuario = usuarioService.cadastrarUsuario(usuariodto);
+            return ResponseEntity.status(HttpStatus.CREATED).body(novoUsuario);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioResponseDTO>> listarUsuarios() {
+        return ResponseEntity.ok(usuarioService.buscarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioResponseDTO> buscarPorId(@PathVariable Long id) {
+        try {
+            return ResponseEntity.ok(usuarioService.buscarPorId(id));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> atualizarUsuario(@PathVariable Long id, @Valid @RequestBody UsuarioRequestDTO dto) {
+        try {
+            UsuarioResponseDTO usuarioAtualizado = usuarioService.atualizarUsuario(id, dto);
+            return ResponseEntity.ok(usuarioAtualizado);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
