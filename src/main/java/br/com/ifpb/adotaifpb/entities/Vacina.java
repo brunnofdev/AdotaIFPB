d0f9a274a2687ce615ed9030fc831e6b26e3cd33
jarthurdev <jarthurdevv@gmail.com>
2026-05-19 18:22:12 -0300

    feat: implementação da entidade Vacina

---
+++ src/main/java/br/com/ifpb/adotaifpb/entities/Vacina.java
@@ -0,0 +1,20 @@
+package br.com.ifpb.adotaifpb.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "tb_vacina")
+public class Vacina {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String nome;
+
+    @Column(nullable = true)
+    private String fabricante;
+}
