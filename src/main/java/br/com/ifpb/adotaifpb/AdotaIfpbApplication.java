947c66c9b01868f9db2691e23e984e8d6f8c00f0
Brunno Felipe Bezerra <brunnofdev@gmail.com>
2026-05-21 17:37:27 -0300

    feat: implementa SecurityConfig e AutenticacaoService

--- src/main/java/br/com/ifpb/adotaifpb/AdotaIfpbApplication.java
+++ src/main/java/br/com/ifpb/adotaifpb/AdotaIfpbApplication.java
@@ -1,16 +1,7 @@
 package br.com.ifpb.adotaifpb;
 
-import br.com.ifpb.adotaifpb.entities.Adocao;
-import br.com.ifpb.adotaifpb.entities.Animal;
-import br.com.ifpb.adotaifpb.entities.Usuario;
-import br.com.ifpb.adotaifpb.repository.AdocaoRepository;
-import br.com.ifpb.adotaifpb.repository.AnimalRepository;
-import br.com.ifpb.adotaifpb.repository.UsuarioRepository;
-import br.com.ifpb.adotaifpb.utils.StatusEnum;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AdotaIfpbApplication {
