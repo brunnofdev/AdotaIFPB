c6d8206e0f52767e52935f3a3c6728011c8355c3
Brunno Felipe Bezerra <brunnofdev@gmail.com>
2026-05-28 20:47:06 -0300

    feat: Implementa Repository de entidade Solicitação

---
+++ src/main/java/br/com/ifpb/adotaifpb/repository/SolicitacaoRepository.java
@@ -0,0 +1,12 @@
+package br.com.ifpb.adotaifpb.repository;
+
+import br.com.ifpb.adotaifpb.entities.Solicitacao;
+import br.com.ifpb.adotaifpb.utils.StatusSolicitacaoEnum;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SolicitacaoRepository extends JpaRepository<Solicitacao, Long> {
+    List<Solicitacao> findAllByAnimalIdAndStatus(Long animalId, StatusSolicitacaoEnum status);
+}
+
