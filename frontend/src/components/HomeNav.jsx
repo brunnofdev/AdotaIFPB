5787c75efd76bccc60bf3f3b9e4a65fad6ec58f1
Brunno Felipe Bezerra <brunnofdev@gmail.com>
2026-07-15 20:24:58 -0300

    feat: cria componente para menu de navegação do Home

---
+++ frontend/src/components/HomeNav.jsx
@@ -0,0 +1,36 @@
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../contexts/useAuth';
+
+export function HomeNav(){
+
+const navigate = useNavigate();
+const { logout } = useAuth();
+
+const handleLogout = () => {
+    logout();
+    navigate('/login');
+  };
+return (
+<nav className="side-navbar">
+  <h3 className="side-navbar-brand">AdotaIFPB</h3>
+    <div className="side-navbar-links">
+      <div className="nav-link">
+        <img src="src/assets/home.png" alt="Home Icon" className="side-nav-icon" />
+        <Link to="/home" className="side-nav-link side-nav-link-active">Início</Link>
+      </div>
+      <div className="nav-link">
+        <img src="src/assets/animal.png" alt="Animal Icon" className="side-nav-icon" />
+        <Link to="/animais" className="side-nav-link">Animais</Link>
+      </div>
+      <div className="nav-link">
+        <img src="src/assets/user.png" alt="User Icon" className="side-nav-icon" />
+        <Link to="/usuarios" className="side-nav-link">Usuários</Link>
+      </div>
+      <div className="nav-link">
+        <img src="src/assets/hearth.png" alt="Adoption Icon" className="side-nav-icon" />
+        <Link to="/solicitacoes" className="side-nav-link">Adoções</Link>
+      </div>
+    </div>
+  <button className="logout-button" onClick={handleLogout}>Sair</button>
+</nav>
+)}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,46 +1,18 @@
-import { useAuth } from '../contexts/useAuth';
-import { Link, useNavigate } from 'react-router-dom';
+import {useNavigate } from 'react-router-dom';
 import { Card } from '../components/Card';
 import '../styles/Home.css'; 
+import { HomeNav } from '../components/HomeNav';
 
 function Home() {
+
   const navigate = useNavigate();
-
-  const { logout } = useAuth();
-
-  const handleLogout = () => {
-    logout();
-    navigate('/login');
-  };
-
   const handleCardClick = (animalId) => {
     navigate('/cadastrar-solicitacao', { state: { animalId } }); 
   }
 
   return (
     <div className="home-container">
-      <nav className="side-navbar">
-      <h3 className="side-navbar-brand">AdotaIFPB</h3>
-        <div className="side-navbar-links">
-          <div className="nav-link">
-            <img src="src/assets/home.png" alt="Home Icon" className="side-nav-icon" />
-            <Link to="/home" className="side-nav-link side-nav-link-active">Início</Link>
-          </div>
-          <div className="nav-link">
-            <img src="src/assets/animal.png" alt="Animal Icon" className="side-nav-icon" />
-            <Link to="/animais" className="side-nav-link">Animais</Link>
-          </div>
-          <div className="nav-link">
-            <img src="src/assets/user.png" alt="User Icon" className="side-nav-icon" />
-            <Link to="/usuarios" className="side-nav-link">Usuários</Link>
-          </div>
-          <div className="nav-link">
-            <img src="src/assets/hearth.png" alt="Adoption Icon" className="side-nav-icon" />
-            <Link to="/solicitacoes" className="side-nav-link">Adoções</Link>
-          </div>
-        </div>
-        <button className="logout-button" onClick={handleLogout}>Sair</button>
-      </nav>
+      <HomeNav />
 
       <div className="content-wrapper">
         <div className="user-info">
